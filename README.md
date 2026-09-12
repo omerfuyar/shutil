67895db02bccda5e236963e7bce46537355d9b2f
omerfuyar <denemeyo@gmail.com>
2026-09-12 23:07:27 +0900

    -planned structure

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Shutil
-Shutil (SHU uTILities) is a portable, cross platform programming utility / data structure library.
+Shutil (SHU uTILities) is a portable, cross platform programming utility library.
 
 It uses the [SHU](https://github.com/omerfuyar/shu) system. By defining `SHU` you can tell the library where to find `shu.h` or include it yourself  before any shu... library to prevent any complication. See [SHU](https://github.com/omerfuyar/shu) repo for more information.
 
 Goal is to use most common data structures with a united, easy interface.
+
+# Usage
+You can pick which data structures or algorithms you will use by defining corresponding `SHUTIL_<...>` macro. Or use the `SHUTIL_ALL` macro for everything that the library provides. See first lines of the header for more information.
--- shutil.h
+++ shutil.h
@@ -1,3 +1,16 @@
+// SHUTIL_ALL : enables all data structures
+// SHUTIL_ARRAY : enables dynamic array data structure
+// SHUTIL_LIST : enables linked list data structure
+// SHUTIL_STACK : enables stack data structure
+// SHUTIL_QUEUE : enables queue data structure
+// SHUTIL_DEQUE : enables double ended queue data structure
+// SHUTIL_MAP : enables hash map data structure
+// SHUTIL_SET : enables hash set data structure
+// SHUTIL_TREE : enables tree data structure
+// SHUTIL_GRAPH : enables graph data structure
+
+// todo algorithms
+
 #pragma once
 
 #ifndef SHU_HEADER
@@ -10,15 +23,99 @@
 
 #pragma region Macros
 
+#ifdef SHUTIL_ALL
+#define SHUTIL_ARRAY
+#define SHUTIL_LIST
+#define SHUTIL_STACK
+#define SHUTIL_QUEUE
+#define SHUTIL_DEQUE
+#define SHUTIL_MAP
+#define SHUTIL_SET
+#define SHUTIL_TREE
+#define SHUTIL_GRAPH
+#endif
+
 #pragma endregion Macros
 
 #pragma region Declarations
+
+#ifdef SHUTIL_ARRAY
+
+#endif
+
+#ifdef SHUTIL_LIST
+
+#endif
+
+#ifdef SHUTIL_STACK
+
+#endif
+
+#ifdef SHUTIL_QUEUE
+
+#endif
+
+#ifdef SHUTIL_DEQUE
+
+#endif
+
+#ifdef SHUTIL_MAP
+
+#endif
+
+#ifdef SHUTIL_SET
+
+#endif
+
+#ifdef SHUTIL_TREE
+
+#endif
+
+#ifdef SHUTIL_GRAPH
+
+#endif
 
 #pragma endregion Declarations
 
 #pragma region Definitions
 
 #ifdef SHU_IMPLEMENTATION
+
+#ifdef SHUTIL_ARRAY
+
+#endif
+
+#ifdef SHUTIL_LIST
+
+#endif
+
+#ifdef SHUTIL_STACK
+
+#endif
+
+#ifdef SHUTIL_QUEUE
+
+#endif
+
+#ifdef SHUTIL_DEQUE
+
+#endif
+
+#ifdef SHUTIL_MAP
+
+#endif
+
+#ifdef SHUTIL_SET
+
+#endif
+
+#ifdef SHUTIL_TREE
+
+#endif
+
+#ifdef SHUTIL_GRAPH
+
+#endif
 
 #endif
 
